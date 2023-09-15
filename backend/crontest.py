ce78f258c76ee7d8540f1f95c862adc1cdcc82a6
blacksmith31 <jamesonhmarshall@gmail.com>
2023-09-15 13:03:07 -0600

    updated schedule trigger and logger

--- backend/crontest.py
+++ backend/crontest.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-def main():
+def crontest():
     print(f"{datetime.now()}: This is the test script running")
 
 if __name__ == "__main__":
-    main()
+    crontest()
     
--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,25 @@
-import logging
-from fastapi import FastAPI
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+import datetime
+from fastapi import FastAPI
+import logging
 
 from . import db
+from backend.crontest import crontest
 from backend.scraper import scrape
 
 logger = logging.getLogger(__name__)
+logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.DEBUG)
 app = FastAPI()
 
 
 @app.on_event("startup")
 def startup():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(scrape, CronTrigger.from_crontab('0 21 * * 1-5'))
+    trigger = CronTrigger(year='*', month='*', day='*', day_of_week='mon-fri', hour='21', minute='0', timezone="US/Eastern")
+    # scheduler.add_job(scrape, CronTrigger.from_crontab('0 21 * * 1-5'))
+    scheduler.add_job(scrape, trigger=trigger, name="Scraper")
     scheduler.start()
 
 @app.get("/")
@@ -26,7 +32,7 @@ def get_tickers(limit: int = 30):
     # print(result)
     return result
 
-@app.get("/ticker/{symbol}")
+@app.get("/tickers/{symbol}")
 def get_ticker(symbol: str):
     result = db.get_ticker(symbol)
     return result
